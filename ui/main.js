3530d2cb23c7de5deda5264041fa156c3e2788a0
Aditya R Dharamshi <30700256+Aditya-Dharamshi@users.noreply.github.com>
2017-08-21 12:17:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img=document.getELementById('madi');
+var marginRight=0;
+function moveLeft(){
+    marginRight=marginRight+10;
+    img.style.marginRight = marginRight+'px';
+}
 img.onclick=function()
 {
     var interval=setIntervalI(moveLeft,100);
